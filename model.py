4e9039de75224d75629f65a9bda280a84f5af684
Maurits Bos <mb.brains@gmail.com>
2022-05-18 15:41:56 +0200

    added start of hyperparameter optimisation

--- model.py
+++ model.py
@@ -7,35 +7,18 @@ from sklearn.model_selection import GroupShuffleSplit
 from torch.cuda import is_available as gpu_is_available
 
 import evaluate
+from utils import split_train_data
 
 
 
-def split_train_data(train_df: DataFrame):
-    gss = GroupShuffleSplit(test_size=.20, n_splits=1, random_state = 7).split(train_df, groups=train_df['srch_id'])
-
-    X_train_inds, X_test_inds = next(gss)
-
-    train_data=  train_df.iloc[X_train_inds]
-    X_train = train_data.loc[:, ~train_data.columns.isin(['scores'])]
-    y_train = train_data.loc[:, train_data.columns.isin(['scores'])]
-
-    groups_train = train_data.groupby('srch_id').size().to_frame('size')['size'].to_numpy()
-
-    test_data= train_df.iloc[X_test_inds]
-
-#We need to keep the id for later predictions
-    X_test = test_data.loc[:, ~test_data.columns.isin(["scores"])]
-    y_test = test_data.loc[:, test_data.columns.isin(['scores'])]
-
-    groups_val = test_data.groupby('scores').size().to_frame('size')['size'].to_numpy()
-    return X_train, y_train, X_test, y_test ,groups_train, groups_val,test_data
 
 #%%
 train_df = pd.read_csv("./data/processed_training_set_Vu_DM.csv", index_col=0)
 
 X_train, y_train, X_val, y_val , groups_train, groups_val, test_data= split_train_data(train_df)
 
-datenow = datetime.now().strftime("%m/%d/%Y%H:%M")
+#%%
+datenow = datetime.now().strftime("%m-%d-%Y-%H-%M")
 savedir = "models/xgb_ranker_"+ datenow+ ".json"
 #%%
 # training and defining model
@@ -56,18 +39,12 @@ model = xgb.XGBRanker(
     )
 
 model.fit(X_train, y_train, group=groups_train, verbose=True)
+
+#%%
 model.save_model(savedir)
 #%%
+# predicting on validation set
 
-# def predict(model, df):
-#     return model.predict(df.loc[:, ~df.columns.isin(['srch_id',"scores"])])
-  
-# predictions = (test_data.groupby('srch_id')
-#                .apply(lambda x: predict(model, x)))
-
-
-
-# X_val_run = X_val.drop("")
 y_val_predict = model.predict(X_val)
 X_val_res = X_val.loc[:, ["srch_id", "prop_id"]]
 #%%
@@ -104,5 +81,5 @@ df["predict"] = model.predict(test_df.loc[:, ~test_df.columns.isin(["srch_id"])]
 # df.sort_values(by=["srch_id", "predict"], ascending=False)
 df.sort_values(by=["srch_id", "predict"], ascending=[True,False])
 # %%
-df[["srch_id", "prop_id"]].to_csv("predictions/VU-DM-2022-Group-155.csv", index=False)
+df[["srch_id", "prop_id"]].to_csv(f"predictions/VU-DM-2022-Group-155-pred{datenow}.csv", index=False)
 # %%
